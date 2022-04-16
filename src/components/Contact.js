41ffb0cfc28c2240460e16d33ff50686244fb5f6
CordiiT14 <cordelia.toennies@live.com>
2022-04-16 09:57:38 +0100

    Contact Form sends form details to email and provides confirmation email.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
@@ -7,19 +8,35 @@ const Contact = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
 
+    const form = useRef();
+
 
     const handleSubmit = (event) => {
-        event.preventdefault()
+        event.preventDefault();
+        sendEmail();
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
+    const sendEmail = () => {
+        console.log(name, email, message);
+        emailjs.sendForm('default_service', 'template_vmxya9h', form.current, 'cvJY15YykIx2Yox-i')
+        .then((result) => {
+            console.log('Success!', result.text);
+        }, (error) => {
+            console.log('Failed...', error.text);
+        });
     }
 
     const handleName = (event) => {
-        let fname = (event.target.value).trim;
+        let fname = (event.target.value);
         setName(fname);
     }
 
 
     const handleEmail = (event) => {
-        let email = (event.target.value).trim;
+        let email = (event.target.value);
         setEmail(email);
     }
 
@@ -32,14 +49,14 @@ const Contact = () => {
         <section id='contact-container'>
             <div className="contact-form">
                 <h3>Get in touch: </h3>
-                <form onSubmit={handleSubmit}>
+                <form ref={form} onSubmit={handleSubmit}>
                     <label>Full Name:* </label>
-                        <input onChange={handleName} id="name" name="name" type="text" value={name} placeholder="Fullname..." required></input>        
+                        <input onChange={handleName} id="from_name" name="from_name" type="text" value={name} placeholder="Fullname..." required></input>        
                     <label>Email:* </label>
-                        <input onChange={handleEmail} id="email" name="email" type="email" value={email}placeholder="Email..." required ></input>
+                        <input onChange={handleEmail} id="email" name="reply_to" type="email" value={email}placeholder="Email..." required ></input>
                     <label>Message: </label>
-                        <textarea onChange={handleText} id="message" value={message}></textarea>
-                    <input type="submit" value="send"></input>
+                        <textarea onChange={handleText} id="message" name="message" value={message}></textarea>
+                    <input type="submit" value="Send"></input>
                 </form>
             </div>
         </section>
