342d62d8d8fd17c05d025e62d392039ae7afccbf
CordiiT14 <cordelia.toennies@live.com>
2022-04-16 12:34:17 +0100

    started to add media queries, header/about section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import './container/CordiiTCreative.css';
+import './container/altmedia.css';
 import CordiiTCreative from './container/CordiiTCreative';
 
 function App() {
