1134890b052f0d2af4f9a236ecf941d8278adc62
CordiiT14 <cordelia.toennies@live.com>
2022-04-16 11:17:01 +0100

    hamburger menu functionality added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,39 +3,36 @@ import {Link} from 'react-scroll';
 
 
 
-const Navbar = () => {
+const Navbar = ({isOpen, setIsOpen }) => {
 
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
 
 
     return(
-        <section id='navbar'>
             <div className="navigation">
-                <li>
-                    <Link activeClass="active" to="about-section" spy={true} smooth={true}>
+                <li >
+                    <Link onClick={handleClick} activeClass="active" to="about-section" spy={true} smooth={true}>
                         About
                     </Link>
                 </li>
                 <li>
-                    <Link to="project-container" spy={true} smooth={true}>
+                    <Link onClick={handleClick} to="project-container" spy={true} smooth={true}>
                     Projects
                     </Link>
                 </li>
                 <li>
-                    <Link to="testemonials-container" spy={true} smooth={true}>
+                    <Link onClick={handleClick} to="testemonials-container" spy={true} smooth={true}>
                     Testemonials
                     </Link>
                 </li>
                 <li>
-                    <Link to="contact-container" spy={true} smooth={true}>
+                    <Link onClick={handleClick} to="contact-container" spy={true} smooth={true}>
                     Contact
                     </Link>
                 </li>
             </div>
-            <div className='TEMP'>
-            <h2>!!This Website is under Construction!!</h2>
-
-            </div>
-        </section>
     )
 
 };
--- src/container/CordiiTCreative.js
+++ src/container/CordiiTCreative.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../components/Navbar';
 import About from '../components/About';
 import ProjectList from './ProjectList';
 import TestimonialList from './TestimonialList';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import burgermenu from '../images/burgerclosed.png'
+import closemenu from '../images/closemenu.png'
 
 const CordiiTCreative = () => {
+    const [isOpen, setIsOpen] = useState('false');
+
+
 
     return(
         <div>
-            <Navbar />
+            <section id='navbar'>
+                {isOpen ? 
+                <img id="burgerIcon" src={burgermenu} onClick={() => {setIsOpen(!isOpen)}}></img> : 
+                <div>
+                    <img id="closeMenu" src={closemenu} onClick={() => {setIsOpen(!isOpen)}}></img>
+                    <Navbar isOpen={isOpen} setIsOpen={setIsOpen}/>
+                </div> }
+            </section>
             <About />
             <ProjectList />
             <TestimonialList />
