4249f8a9cf4528fc1df9ffed57b655621a736126
CordiiT14 <cordelia.toennies@live.com>
2022-05-07 16:38:19 +0100

    project section updated

--- src/container/ProjectList.js
+++ src/container/ProjectList.js
@@ -25,8 +25,10 @@ const ProjectList = () => {
                 </a>
                 <img className='project-img' src={pomodoro}></img>
                 <p>Study aid app with Pomodoro Timer</p>
-                <img className='tech-stack' src={react} alt='React'></img>
-                <img className='tech-stack' src={js} alt='JavaScript'></img>
+                <section className='stack-logos'>
+                    <img className='tech-stack' src={react} alt='React'></img>
+                    <img className='tech-stack' src={js} alt='JavaScript'></img>
+                </section>
             </div>
             <div className='project'>
                 <a href="https://github.com/CLg003/TrioLingo" target="_blank">
@@ -34,9 +36,11 @@ const ProjectList = () => {
                 </a>
                 <img className='project-img' src={triolingo}></img>
                 <p>A prototype of an educational language app for kids</p>
-                <img className='tech-stack' src={react} alt='React'></img>
-                <img className='tech-stack' src={js} alt='JavaScript'></img>
-                <img className='tech-stack' src={mongodb} alt='MongoDB'></img>
+                <section className='stack-logos'>
+                    <img className='tech-stack' src={react} alt='React'></img>
+                    <img className='tech-stack' src={js} alt='JavaScript'></img>
+                    <img className='tech-stack' src={mongodb} alt='MongoDB'></img>
+                </section>
             </div>
             <div className='project'>
                 <a href="https://github.com/CordiiT14/w7_hw_weather_app" target="_blank">
@@ -44,8 +48,11 @@ const ProjectList = () => {
                 </a>
                 <img className='project-img' src={weather}></img>
                 <p>React Weather App using API for weather data</p>
-                <img className='tech-stack' src={react}></img>
-                <img className='tech-stack' src={js} alt='JavaScript'></img>
+                <section className='stack-logos'>
+                    <img className='tech-stack' src={react}></img>
+                    <img className='tech-stack' src={js} alt='JavaScript'></img>
+                </section>
+
             </div>
             <div className='project'>
                 <a href="https://github.com/CordiiT14/python_project_bookings_manager" target="_blank">
@@ -53,9 +60,12 @@ const ProjectList = () => {
                 </a>
                 <img className='project-img' src={events}></img>
                 <p>Booking system to add customers to events</p>
-                <img className='tech-stack' src={python} alt='Python'></img>
-                <img className='tech-stack' src={flask} alt='Flask'></img>
-                <img className='tech-stack' src={postgrSQL} alt='PostgreSQL'></img>
+                <section className='stack-logos'>
+                    <img className='tech-stack' src={python} alt='Python'></img>
+                    <img className='tech-stack' src={flask} alt='Flask'></img>
+                    <img className='tech-stack' src={postgrSQL} alt='PostgreSQL'></img>
+                </section>
+
             </div>
             <div className='project'>
                 <a href="https://github.com/CordiiT14/wk3_hw_Flask_rockpaperscissors" target="_blank">
@@ -63,8 +73,10 @@ const ProjectList = () => {
                 </a>
                 <img className='project-img' src={rps}></img>
                 <p>Rock Paper Scissors game. Play with a friend or against computer</p>
-                <img className='tech-stack' src={python} alt='Python'></img>
-                <img className='tech-stack' src={flask} alt='Flask'></img>
+                <section className='stack-logos'>
+                    <img className='tech-stack' src={python} alt='Python'></img>
+                    <img className='tech-stack' src={flask} alt='Flask'></img>
+                </section>
             </div>
         </section>
     )
