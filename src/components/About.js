369040d1e45d7a8c1eba06d81bf9f5cedac94cb4
CordiiT14 <cordelia.toennies@live.com>
2022-06-13 16:14:31 +0100

    updating about me section

--- src/components/About.js
+++ src/components/About.js
@@ -11,15 +11,15 @@ const About = () => {
         <>
             <div id='about-header'>
                 <img  src={logo} alt="Cordii T Creative Logo"></img>
-                <h2> Hi, my name is Cordelia. I am a junior full-stack developer, digital illustrator and photographer.</h2>
+                <h2> Hi, my name is Cordelia. I am a junior full-stack developer and digital illustrator.</h2>
             </div>
             <div id="background-image"></div>
             <section className='about-section'>
                 <img id="headshot" src={headshot} alt="Cordelia"></img>
                 <section>
-                    <p>I have been a freelance photographer since 2012 and a digital illustrator since 2020.</p>
-                    <p>My creative side is matched by a proclivity for problem solving and logical thinking. Which has brought me to develop and build my skillset as a software developer.</p>
-                    <p>Since February 2022 I have been attending the CodeClan Professional Software Development course.</p>
+                    <p>I moved from Germany to Edinburgh in 2011 for my undergraduate in Drama and Performance. Over the last ten years I have worked in hospitality, retail management and the performaning arts.</p>
+                    <p>At the start of 2022 I made the decision to retrain as a Software Developer with CodeClan's 16-week intensive course.</p>
+                    <p>Scroll down to see some of my work from the past 4 months or checkout my GitHub.</p>
                     <div id="social-media">
                         <a href="https://www.linkedin.com/in/cordelia-toennies-johnstone-2a1148102" target="_blank" rel="noreferrer">LinkedIn</a>
                         <a href="https://github.com/CordiiT14" target="_blank" rel="noreferrer">Github</a>
